c384d382b0a79c35b036fbd3984bad8761a871e1
Hoid <github@oedon.me>
2023-05-23 16:26:33 +0200

    feat: update

--- flake.nix
+++ flake.nix
@@ -1,15 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
-    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, ... }:
     let
       system = "x86_64-linux";
       overlay-unstable = final: prev: {
-        # unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-        # use this variant if unfree packages are needed:
         unstable = import nixpkgs-unstable {
           inherit system;
           config.allowUnfree = true;
@@ -19,7 +17,6 @@
     {
       nixosConfigurations."3900X" = nixpkgs.lib.nixosSystem {
         inherit system;
-        # specialArgs = attrs;
         modules = [
           # Overlays-module makes "pkgs.unstable" available in configuration.nix
           ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
