9a31a9f2dd92b473d98c55a146145eb14ba9d1df
Hoid <github@oedon.me>
2025-11-28 21:26:22 +0100

    chore: update

--- services.nix
+++ services.nix
@@ -36,14 +36,6 @@
       drivers = [ pkgs.samsung-unified-linux-driver_1_00_37 ];
     };
 
-    # Locate Service
-    # locate = {
-    #   enable = false;
-    #   package = pkgs.mlocate;
-    #   interval = "hourly";
-    #   localuser = null;
-    # };
-
     # TRIM
     fstrim = {
       enable = true;
@@ -52,34 +44,13 @@
 
     flatpak.enable = true;
 
+    colord.enable = true;
+
     # Start emacs daemon
     emacs = {
       enable = true;
       # package = pkgs.emacs29;
     };
-
-    # ollama = {
-    #   enable = true;
-    #   acceleration = "rocm";
-    #   package = pkgs.unstable.ollama;
-    #   # listenAddress = "10.0.0.97:11434";
-    #   environmentVariables = {
-    #     HSA_OVERRIDE_GFX_VERSION = "11.0.2";
-    #   };
-    # };
-    # open-webui = {
-    #   enable = true;
-    #   host = "10.0.0.97";
-    #   port = 8080;
-    #   # package = pkgs.unstable.open-webui;
-    #   # environment = {
-    #   #   ANONYMIZED_TELEMETRY = "False";
-    #   #   DO_NOT_TRACK = "True";
-    #   #   SCARF_NO_ANALYTICS = "True";
-    #   #   OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-    #   #   OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-    #   # };
-    # };
 
     # Keyring
     gnome.gnome-keyring.enable = true;
--- xserver.nix
+++ xserver.nix
@@ -5,49 +5,34 @@
       enable = true;
       videoDrivers = [ "nvidia" ];
 
-      # Configure keymap in X11
-      xkb.layout = "us,us";
-      xkb.variant = ",intl";
+      xkb.layout = "us";
+      # xkb.variant = ",intl";
       # xkbOptions = "grp:shifts_toggle, grp_led:caps, caps:escape";
+
       autoRepeatDelay = 300;
       autoRepeatInterval = 50;
-
-      # Desktop managers
-      # desktopManager = {
-      #   gnome.enable = true;
-      #   plasma6.enable = true;
-      # };
     };
 
-    # Login Display settings
     displayManager = {
-      # startx.enable = true;
       gdm.enable = true;
-      gdm.wayland = true;
       gdm.autoSuspend = false;
       defaultSession = "hyprland";
-
-      # sessionCommands = ''
-      #   xrandr --output DP-2 --mode 3440x1440 --rate 144
-      #   xset r rate 300 50 &
-      #   xset m 0 0 &
-      #   dunst &
-      #   nitrogen --restore &
-      #   picom &
-      #   dwmblocks &
-      #   unclutter &
-      # '';
-
     };
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gnome
+      pkgs.xdg-desktop-portal-hyprland
+    ];
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
   };
 
   programs.waybar.enable = true;
-
-  programs.hyprland = {
-    # Install the packages from nixpkgs
-    enable = true;
-    # Whether to enable XWayland
-    xwayland.enable = true;
-  };
 
 }
